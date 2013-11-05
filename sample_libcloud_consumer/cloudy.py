36ad207d6ae22e33e0966d94bcb7250108f73d9a
Quinn Slack <qslack@qslack.com>
2013-11-04 16:46:49 -0800

    cloudy

--- sample_libcloud_consumer/cloudy.py
+++ sample_libcloud_consumer/cloudy.py
@@ -2,14 +2,18 @@ import os
 from libcloud.storage.types import Provider
 from libcloud.storage.providers import get_driver
 
-S3_ACCESS_ID = os.getenv('AWS_ACCESS_KEY_ID')
-S3_SECRET_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+class Cloudy(object):
+    def cloudy(self):
+        S3_ACCESS_ID = os.getenv('AWS_ACCESS_KEY_ID')
+        S3_SECRET_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 
-Driver = get_driver(Provider.S3)
-conn = Driver(S3_ACCESS_ID, S3_SECRET_KEY)
+        Driver = get_driver(Provider.S3)
+        conn = Driver(S3_ACCESS_ID, S3_SECRET_KEY)
 
-containers = conn.list_containers()
-print('%d containers:' % len(containers))
-for container in containers:
-    print(' - %s' % container.name)
+        containers = conn.list_containers()
+        print('%d containers:' % len(containers))
+        for container in containers:
+            print(' - %s' % container.name)
 
+if __name__ == '__main__':
+    Cloudy().cloudy()
