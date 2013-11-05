3dc823176a6cd5e02ff59634e38e2b210558cc53
Quinn Slack <qslack@qslack.com>
2013-11-04 16:35:59 -0800

    try this?

--- setup.py
+++ setup.py
@@ -1,15 +1,5 @@
 from setuptools import setup
 
-requires=[
-    'apache-libcloud'
-]
-install_requires = []
-for package in requires:
-    try:
-        import package
-    except ImportError:
-        install_requires.append(package)
-
 setup(
     name = 'sample-libcloud-consumer',
     packages = ['sample_libcloud_consumer'],
@@ -18,5 +8,5 @@ setup(
     author = 'Quinn Slack',
     author_email = 'sqs@sourcegraph.com',
     url = 'https://github.com/sourcegraph/sample-libcloud-consumer',
-    install_requires = install_requires,
+    install_requires = ['apache-libcloud'],
 )
